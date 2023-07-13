1b861e5df6a894509bbb0c60a177d5f5518cbe6b
Matli Mahender Reddy <102343517+MatliMahenderReddy@users.noreply.github.com>
2023-07-13 12:19:48 +0530

    Add files via upload

---
+++ DvpServlet.java
@@ -0,0 +1,49 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.cj.protocol.Resultset;
+@WebServlet("/vdp")
+public class DvpServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital","root","root");
+			PreparedStatement ps=con.prepareStatement("select * from dp");    	
+	    	ResultSet rs=ps.executeQuery();
+	    	PrintWriter pw=resp.getWriter();
+	    	
+	    	pw.println(" Patients Deatils ");
+	    	  
+	    	pw.print(" ID   "+ "         Name "+"        Dicese "+"         Place "+"       Days "+"     Bill ");
+	    	pw.println("");
+			while(rs.next())
+			{
+				 pw.print("  "+ rs.getInt(1)+" ");
+				   pw.print("           "+rs.getString(2)+" ");
+				   pw.print("       "+rs.getString(3)+" ");
+				   pw.print("      "+rs.getString(4)+" ");
+				   pw.print("      "+rs.getInt(5)+" ");
+				   pw.print("        "+rs.getInt(6)+" ");
+				   pw.println("                      ");
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
